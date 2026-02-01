cac0398db1743b9fed2e889ea925d526eee77a00
El-Mavericko <michaelbands@yahoo.com>
2026-02-01 22:47:58 +0000

    Update and rename main.jsx to main.tsx
    updated script from javascript to typescript for main file

---
+++ main.tsx
@@ -0,0 +1,15 @@
+// main.tsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const container = document.getElementById('root');
+
+if (!container) throw new Error("Root container missing in index.html");
+
+createRoot(container).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
